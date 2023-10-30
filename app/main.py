0280873bf712e5035914b9f61cb0f787b1dde4c7
humphrey909 <humphrey1858@gmail.com>
2023-10-30 17:30:48 +0900

    fix:env 추가

--- app/main.py
+++ app/main.py
@@ -50,4 +50,4 @@ def create_app():
 app = create_app()
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8085, reload=True)
