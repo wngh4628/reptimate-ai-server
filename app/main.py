32dc746013b648903419a96ef1355d368007bed7
humphrey909 <humphrey1858@gmail.com>
2023-11-27 18:46:51 +0900

    FIX:포트 8000으로 변경

--- app/main.py
+++ app/main.py
@@ -57,4 +57,4 @@ def create_app():
 app = create_app()
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8050, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
