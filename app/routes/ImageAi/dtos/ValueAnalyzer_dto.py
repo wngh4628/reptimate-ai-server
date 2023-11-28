35b6ed10c908dde2a4b37a175a540eae2ff74366
문준호 <wngh46280@gmail.com>
2023-11-28 22:16:22 +0900

    Fix: 레터럴 100점 이상 수정12

--- app/routes/ImageAi/dtos/ValueAnalyzer_dto.py
+++ app/routes/ImageAi/dtos/ValueAnalyzer_dto.py
@@ -3,20 +3,20 @@ import json
 from typing import Optional
 class ValueAnalyzerCreate(BaseModel):
     user_idx: Optional[int] = None
-    pet_name: str
-    morph: str
-    gender: str
-    head_score: int
-    dorsal_score: int
-    tail_score: int
-    left_score: int
-    right_score: int
-    total_score: int
-    left_info: str
-    right_info: str
-    top_img: str
-    left_img: str
-    right_img: str
+    pet_name: str = '릴리'
+    morph: str = '릴리 화이트'
+    gender: str = '암컷'
+    head_score: int = 60
+    dorsal_score: int = 80
+    tail_score: int = 100
+    left_score: int = 80
+    right_score: int = 80
+    total_score: int = 80
+    left_info: str = '{"score": 50, "SecondPercent": 10.444589272052365, "ThirdPercent": 0.0, "RGB": [[79, 47, 0], [151, 129, 80]]}'
+    right_info: str = '{"score": 50, "SecondPercent": 10.444589272052365, "ThirdPercent": 0.0, "RGB": [[79, 47, 0], [151, 129, 80]]}'
+    top_img: str = '1'
+    left_img: str = '1'
+    right_img: str = '1'
 
     @staticmethod
     def updateFrom(
